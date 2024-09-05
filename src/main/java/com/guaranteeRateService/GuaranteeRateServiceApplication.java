a8060f30035281105a972cb858b28f2c160bad44
Shabbir khan <33508921+shabbirkhan0015@users.noreply.github.com>
2024-09-05 17:07:05 +0530

    first commit

---
+++ src/main/java/com/guaranteeRateService/GuaranteeRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.guaranteeRateService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaranteeRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuaranteeRateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guaranteeRateService/GuaranteeRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guaranteeRateService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuaranteeRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
